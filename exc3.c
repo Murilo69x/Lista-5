409a7df59fc8d49a776ee89e596fdad64065fc97
Murilo69x <muriloolliveira8@gmail.com>
2026-05-06 20:34:21 -0300

    Create exc3.c

---
+++ exc3.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main() ( int opcao;
+float saldo = 1000.00, deposito;
+do{
+printf("Menu\n"):
+printf("1 - Consultar saldo\n");
+printf("2 - Depositar\n");
+printf("® - Sair\n"');
+scanf (%d", «opcao);
+if (opcao == 1)(
+printf()%f", &saldo);
+Jelse if (opcao == 2)C
+printf("Digite a quantidade"');
+scanf (%f", &deposito);
+Jelse if (opcao == 0)
+printf("programa encerrado");
+}
+while(opcao |=0);
+return 0;
+}
