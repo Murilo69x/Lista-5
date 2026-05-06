2326d02bc4098c1d8422576c6df1c9e447d2b045
Murilo69x <muriloolliveira8@gmail.com>
2026-05-06 19:25:08 -0300

    Create exc1.c

---
+++ exc1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+   int i, codigo;
+   int validos = 0, invalidos = 0;
+   for (i=1; i = 10; i++){
+       scanf("%d", &codigo);
+    if(codigo >= 1000 & codigo <= 9999){
+        printf("codigo valido");
+    }
+    else{
+        printf("codigo valido");}
+    }
+   
+    return 0;
+} 
