c6156d9916fe12bbb0a8a8b7c2a79d9b95cfa20f
Murilo69x <muriloolliveira8@gmail.com>
2026-05-06 19:58:09 -0300

    Create exc2.c

---
+++ exc2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    float meta, valorMes, total = 0;
+    int mesesValidos = 0;
+
+    printf("Digite a meta de economia: ");
+    scanf("%f", &meta);
+
+    while (total < meta) {
+
+        printf("Digite o valor economizado no mes: ");
+        scanf("%f", &valorMes);
+
+        if (valorMes > 0) {
+            total += valorMes;
+            mesesValidos++;
+        } else {
+            printf("Valor desconsiderado\n");
+        }
+    }
+
+    printf("\nTotal economizado: %.2f\n", total);
+    printf("Meses validos: %d\n", mesesValidos);
+
+    return 0;
+}
